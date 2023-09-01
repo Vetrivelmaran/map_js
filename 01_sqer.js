ecf7016db02e43d26ed990a60f0bfe35229cbab1
Vetrivelmaran <vetri4723@gmai.com>
2023-09-01 10:23:19 +0530

    first commit

---
+++ 01_sqer.js
@@ -0,0 +1,9 @@
+
+// Write a function that takes an array of numbers and returns a new array with each
+// number squared. Use the Map object to accomplish this.
+
+
+
+s=[2,3,4,5]
+arr=s.map(x=>x*x);
+console.log(arr)
---
+++ 02_capitalize_name.js
@@ -0,0 +1,5 @@
+const Arr=[{name:'vetri',age:22},{name:'david',age:23},{name:'pravin',age:23}]
+cap_name=Arr.map(x=>
+    { return x.name.toUpperCase()
+    })
+console.log(cap_name)
